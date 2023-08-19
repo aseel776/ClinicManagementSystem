42a07eef29cfc7cd57925796607679e8e884c998
aseel776 <95752507+aseel776@users.noreply.github.com>
2023-08-19 22:19:46 +0200

    initializing

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const lightGreen = Color(0xFFAFC8C6);
+  static const black = Color(0xff252525);
+  static const lightGrey = Color(0xffebebeb);
+  static const lightYellow = Color(0xFFFFF7EB);
+  static const lightBlue = Color(0xFFEFF5F9);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,27 @@
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import './features/appointments_sessions/presentation/widgets/main_section.dart';
 
+void main() async {
+  runApp(const MainPage());
 }
 
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.sizeOf(context).width;
+
+    return ProviderScope(
+      child: MaterialApp(
+        title: "Clinic Management System",
+        home: Directionality(
+          textDirection: TextDirection.rtl,
+          child: AppointmentsMainSection(sectionWidth: screenWidth * 0.8),
+        ),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
